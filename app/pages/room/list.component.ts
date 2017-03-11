321dd32ec020f6b985acab0bf7e6f192318fd367
Mayconn Oliveira <mayconnwgo@gmail.com>
2017-03-11 09:21:08 -0800

    erro

--- app/pages/room/list.component.ts
+++ app/pages/room/list.component.ts
@@ -2,6 +2,8 @@ import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { Room } from "../../shared/room/room";
 import { RoomService } from "../../shared/room/room.service";
 
+import { TnsSideDrawer } from 'nativescript-sidedrawer'
+
 @Component({
 	moduleId: module.id,
   selector: "list",
@@ -10,11 +12,31 @@ import { RoomService } from "../../shared/room/room.service";
   providers: [RoomService]
 })
 export class ListComponent implements OnInit {
-  list: Array<Room> = [];
+  list: Array<Room> = [];  
+  i: number;
 
-  constructor(private service: RoomService) {}
+  constructor(private service: RoomService) {
+
+    TnsSideDrawer.build({
+      templates: [{
+          title: 'Home',
+          androidIcon: 'ic_home_white_24dp',
+          iosIcon: 'ic_home_white',
+      }],
+      title: 'This App Name',
+      subtitle: 'is just as awesome as this subtitle!',
+      listener: (index) => {
+          this.i = index
+      },
+      context: this,
+    })
+
+  }
 
   ngOnInit() {
+
+
+
     this.service.list("1")//???
     .subscribe(list => {      
       list.forEach( item => this.list.unshift(item));
